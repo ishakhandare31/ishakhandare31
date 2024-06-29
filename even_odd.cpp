eceb688ac0b8387734c5e1208a110272318ac0c6
ishakhandare31 <ishaanandakhandare@gmail.com>
2024-06-29 17:41:39 +0530

    Create even_odd.cpp

---
+++ even_odd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    
+    cout<<"Enter a number : ";
+    cin>>n;
+    
+    if(n%2==0)
+    cout<<"The number is Even.";
+    else{
+        cout<<"The number is Odd.";
+    }
+    return 0;
+}
